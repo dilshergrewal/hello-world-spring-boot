4c182951bf9018f4d8bd2f1ea6f4a4ceb5872f6c
dilshergrewal <grewal.sher@gmail.com>
2022-08-26 13:29:53 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class Example {
 
-    @GetMapping("/users")
+    @GetMapping("/home")
     String home() {
         return "Hello World - v3!";
     }
