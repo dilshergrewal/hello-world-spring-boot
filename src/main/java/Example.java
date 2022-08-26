143071f4ed2e0eb3cb30e30b26025e845a68df7c
dilshergrewal <grewal.sher@gmail.com>
2022-08-26 12:38:58 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class Example {
 
-    @RequestMapping("example", method = RequestMethod.GET)
+    @RequestMapping(value = "example", method = RequestMethod.GET)
     String home() {
         return "Hello World - v3!";
     }
