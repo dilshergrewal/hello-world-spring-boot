2c728e31d149611432f047b2e42a30be869d7d81
dilshergrewal <grewal.sher@gmail.com>
2022-08-26 13:32:19 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -3,7 +3,6 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
